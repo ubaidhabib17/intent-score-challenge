42b03d17905275bc21a1df702bfc38335cf7550c
ubaidhabib17 <ubaidhabib17@gmail.com>
2020-02-26 10:51:03 +0700

    masih error

--- app/src/main/java/id/putraprima/skorbola/MatchActivity.java
+++ app/src/main/java/id/putraprima/skorbola/MatchActivity.java
@@ -2,18 +2,98 @@ package id.putraprima.skorbola;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.IOException;
 
 public class MatchActivity extends AppCompatActivity {
+    TextView homeName;
+    TextView awayName;
+    TextView awayScore;
+    TextView homeScore;
+    ImageView homeLogo;
+    ImageView awayLogo;
+    Uri uri1;
+    Uri uri2;
+    Bitmap bitmap1;
+    Bitmap bitmap2;
+    String homeTeam;
+    String awayTeam;
+    String scorerText;
+    int scoreHome = 0;
+    int scoreAway = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_match);
+        homeName = findViewById(R.id.txt_home);
+        awayName = findViewById(R.id.txt_away);
+        homeLogo = findViewById(R.id.home_logo);
+        awayLogo = findViewById(R.id.away_logo);
+        awayScore = findViewById(R.id.score_away);
+        homeScore = findViewById(R.id.score_home);
         //TODO
         //1.Menampilkan detail match sesuai data dari main activity
-        //2.Tombol add score menambahkan memindah activity ke scorerActivity dimana pada scorer activity di isikan nama pencetak gol
-        //3.Dari activity scorer akan mengirim kembali ke activity matchactivity otomatis nama pencetak gol dan skor bertambah +1
-        //4.Tombol Cek Result menghitung pemenang dari kedua tim dan mengirim nama pemenang beserta nama pencetak gol ke ResultActivity, jika seri di kirim text "Draw",
+        Bundle extras = getIntent().getExtras();
+        homeTeam = extras.getString("inputHome");
+        awayTeam = extras.getString("inputAway");
+        scorerText = extras.getString("inputScorer");
+
+        if (extras != null) {
+            // TODO: display value here
+
+            uri1 = Uri.parse(extras.getString("logoHome"));
+            uri2 = Uri.parse(extras.getString("logoAway"));
+            bitmap1 = null;
+            bitmap2 = null;
+
+            try {
+                bitmap1 = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri1);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                bitmap2 = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri2);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            homeName.setText(homeTeam);
+            awayName.setText(awayTeam);
+            homeLogo.setImageBitmap(bitmap1);
+            awayLogo.setImageBitmap(bitmap2);
+
     }
+    }
+
+    //2.Tombol add score menambahkan memindah activity ke scorerActivity dimana pada scorer activity di isikan nama pencetak gol
+    public void scoreHomeHandler(View view){
+        scoreHome += 1;
+        homeScore.setText(String.valueOf(scoreHome));
+        Intent intent = new Intent(this, ScorerActivity.class);
+        intent.putExtra("homeScore", scoreHome);
+        startActivity(intent);
+    }
+
+    public void scoreAwayHandler(View view) {
+        scoreAway +=1;
+        awayScore.setText(String.valueOf(scoreAway));
+        Intent intent = new Intent(this, ScorerActivity.class);
+        intent.putExtra("awayScore", scoreAway);
+        startActivity(intent);
+    }
+
+    //3.Dari activity scorer akan mengirim kembali ke activity matchactivity otomatis nama pencetak gol dan skor bertambah +1
+
+
+    //4.Tombol Cek Result menghitung pemenang dari kedua tim dan mengirim nama pemenang beserta nama pencetak gol ke ResultActivity, jika seri di kirim text "Draw",
 }
--- app/src/main/java/id/putraprima/skorbola/ScorerActivity.java
+++ app/src/main/java/id/putraprima/skorbola/ScorerActivity.java
@@ -1,14 +1,69 @@
 package id.putraprima.skorbola;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import java.io.IOException;
 
 public class ScorerActivity extends AppCompatActivity {
+    EditText scorerText;
+
+
+    private static final String TAG = MainActivity.class.getCanonicalName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scorer);
+        scorerText = findViewById(R.id.scorerName);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_CANCELED){
+            return;
+        }
+
+        if(requestCode == 1){
+            if(data != null){
+                try {
+                    imageUri1 = data.getData();
+                    bitmap1 = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri1);
+                    homeImage.setImageBitmap(bitmap1);
+                }catch (IOException e){
+                    Toast.makeText(this,"Can't Load Image",Toast.LENGTH_SHORT).show();
+                    Log.e(TAG, e.getMessage());
+                }
+            }
+        }else if(requestCode == 2){
+            if(data != null){
+                try {
+                    imageUri2 = data.getData();
+                    bitmap2 = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri2);
+                    awayImage.setImageBitmap(bitmap2);
+                }catch (IOException e){
+                    Toast.makeText(this,"Can't Load Image",Toast.LENGTH_SHORT).show();
+                    Log.e(TAG, e.getMessage());
+                }
+            }
+        }
+    }
+
+    public void handleScorerName(View view) {
+        String inputScorer = scorerText.getText().toString();
+
+        Intent intent = new Intent(this, MatchActivity.class);
+        intent.putExtra("inputScorer", inputScorer);
+        startActivity(intent);
     }
 }
